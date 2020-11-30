052e7b4ae0e54a3fa7ecaf2bef942bed4e937122
devwardhan <devwardhanpathak@gmail.com>
2020-11-30 15:28:17 +0530

    Footer component added and  completed

---
+++ src/components/Footer.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import './css/footer.css';
+import GitHubIcon from '@material-ui/icons/GitHub';
+import LinkedInIcon from '@material-ui/icons/LinkedIn';
+import TwitterIcon from '@material-ui/icons/Twitter';
+import MailIcon from '@material-ui/icons/Mail';
+
+const Footer = () => {
+    return (
+        <>
+            <div className="main-footer">
+                <ul className="footer-icons">
+                    <li><GitHubIcon className="material-icon" /></li>
+                    <li><LinkedInIcon className="material-icon" /></li>
+                    <li><TwitterIcon className="material-icon" /></li>
+                    <li><MailIcon className="material-icon" /></li>
+                </ul>
+            </div>
+        </>
+    );
+};
+export default Footer;
