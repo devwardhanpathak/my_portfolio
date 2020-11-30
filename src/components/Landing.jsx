1f049fe30054eced1ec5db95a3eea2ec978e38df
devwardhan <devwardhanpathak@gmail.com>
2020-11-30 13:55:59 +0530

    Header component completed

---
+++ src/components/Landing.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './css/landing.css';
+const Landing = () => {
+    return (<>
+        <div class="header">
+            <h1>Landing Page</h1>
+        </div>
+    </>
+    );
+};
+export default Landing;
