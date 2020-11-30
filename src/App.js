0adae22d9c99b4d96b685aaab202c0bf5677250a
devwardhan <devwardhanpathak@gmail.com>
2020-11-30 15:27:27 +0530

    Footer component completed

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
 import Landing from './components/Landing.jsx'
-import Header from "./components/Header.jsx";
+import Footer from "./components/Footer.jsx";
 import './App.css';
 
 function App() {
   return (
     <Route>
-      <Header />
+      <Landing />
     </Route>
   );
 }
--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -1,11 +1,17 @@
 import React from 'react';
+import Header from './Header.jsx';
+import Footer from './Footer.jsx';
+import bgpic from './image/download.jpg';
 import './css/landing.css';
 const Landing = () => {
-    return (<>
-        <div class="header">
-            <h1>Landing Page</h1>
-        </div>
-    </>
+    return (
+        <>
+            <div class="header-main">
+                <img className="videobg" src={bgpic}></img>
+                <Header />
+            </div>
+            <Footer />
+        </>
     );
 };
 export default Landing;
