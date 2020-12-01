012f6ff26e50676fe6bd890de2d054ddd3f374da
devwardhan <devwardhanpathak@gmail.com>
2020-12-01 15:13:12 +0530

    Landing compelete and header updated

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,53 +1,22 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
-import my_pic from './image/aboutme.jpeg'
 import './css/header.css';
 
-class Header extends React.Component {
-
-    state = {};
-
-    openNav = () => {
-        if (
-            document.getElementById("sideNav") &&
-            document.getElementById("main_box")
-        ) {
-            document.getElementById("sideNav").style.width = "500px";
-            document.getElementById("main_box").style.marginLeft = "0px";
-        }
-    };
-
-    closeNav = () => {
-        if (
-            document.getElementById("sideNav") &&
-            document.getElementById("main_box")
-        ) {
-            document.getElementById("sideNav").style.width = "0";
-            document.getElementById("main_box").style.marginLeft = "0";
-        }
-    };
-
-
-    render() {
-        return (
-            <>
-                <div className="header-main">
-                    <div className="header-container">
-                        <span className="l-text" id="main_box" onClick={this.openNav}>Devwardhan</span>
-                        <div id="sideNav" className="side_menu">
-                            <img className="img-fluid menu_logo" src={my_pic} />
-                            <Link className="close_btn" href="javascript:void(0)" onClick={this.closeNav}> &times;</Link>
-                        </div>
-                        <ul className="nav-links">
-                            <li><Link className="li-links" to="/" >About Me</Link></li>
-                            <li><Link className="li-links" to="/" >My Work</Link></li>
-                            <li><Link className="li-links" to="/" >Contact Me</Link></li>
-                        </ul>
-                    </div>
+const Header = () => {
+    return (
+        <>
+            <div className="header-main">
+                <div className="header-container">
+                    <span className="l-text">Devwardhan</span>
+                    <ul className="nav-links">
+                        <li><Link className="li-links" to="/" >About Me</Link></li>
+                        <li><Link className="li-links" to="/" >My Work</Link></li>
+                        <li><Link className="li-links" to="/" >Contact Me</Link></li>
+                    </ul>
                 </div>
-            </>
-        );
-    }
+            </div>
+        </>
+    );
 };
 
 export default Header;
--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -1,14 +1,18 @@
 import React from 'react';
 import Header from './Header.jsx';
 import Footer from './Footer.jsx';
-import bgpic from './image/download.jpg';
 import './css/landing.css';
 const Landing = () => {
     return (
         <>
-            <div class="header-main">
-                <img className="videobg" src={bgpic}></img>
+            <div class="landing-main">
                 <Header />
+                <div className="text-div">
+                    <h2 className="greet-tag">Hi, I am</h2>
+                    <h1 className="name-tag">Devwardhan Pathak</h1>
+                    <h2 className="what-tag">Front-End Developer</h2>
+                    <h3 className="tech-tag">JavaScript/CSS/React</h3>
+                </div>
             </div>
             <Footer />
         </>
